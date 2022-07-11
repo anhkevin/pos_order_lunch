61aa81b0699decdcbf67e807a728dd4cf8f5811d
Anh Kevin <anhkevin.ht@gmail.com>
2022-07-12 01:14:09 +0700

    fix order

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -13,39 +13,11 @@
                 <div class="panel-body">
 
                     <div class="row">
-                        <div class="col-lg-12">
-                            <form method="post" action="{{ route('user.orders.store') }}" class="form-horizontal">
+                        
+                        <div class="col-md-12">
+                        
+                                <form method="post" action="{{ route('user.orders.store') }}" class="form-horizontal">
                                 {{ csrf_field() }}
-
-                                <div class="form-group"><label class="col-sm-2 control-label">Đặt cơm</label>
-
-                                    @if ($product_rice->count() > 0)
-                                        <div class="col-sm-10 row-no-gutters">
-                                            @foreach ($product_rice as $product)
-                                                <div class="col-sm-6"><label> <input type="radio" value="{{ $product->id }}" id="product_{{ $product->id }}" name="product_rice"> {{ $product->name }} <span style="color: #ff4d4d;">({{ number_format($product->price, 0, ".", ",") . "đ" }})</span> </label></div>
-                                            @endforeach
-                                        </div>
-                                    @endif
-                                </div>
-                                <div class="hr-line-dashed"></div>
-                                <div class="form-group"><label class="col-sm-2 control-label">Món thêm</label>
-                                    @if ($product_option->count() > 0)
-                                    <div class="col-sm-10">
-                                        @foreach ($product_option as $product)
-                                        <div><label class="checkbox-inline">
-                                            <input type="checkbox" name="toppings[]" value="{{ $product->id }}" id="product_{{ $product->id }}"> {{ $product->name }} <span style="color: #ff4d4d;">({{ number_format($product->price, 0, ".", ",") . "đ" }})</span>
-                                        </label></div>
-                                        @endforeach
-                                    </div>
-                                    @endif
-                                </div>
-                                <div class="hr-line-dashed"></div>
-
-                                <div class="hr-line-dashed"></div>
-                                <div class="form-group"><label class="col-sm-2 control-label">Ghi chú</label>
-
-                                    <div class="col-sm-10"><input type="text" name="instructions" class="form-control"></div>
-                                </div>
 
                                 <div class="hr-line-dashed"></div>
                                 <div class="form-group">
@@ -54,7 +26,11 @@
                                         {{ session('status') }}
                                     </div>
                                 @endif
-                                    <div class="col-sm-6 col-sm-offset-2">
+                                </div>
+                                <div class="form-group">
+                                    <label class="col-sm-2 control-label">Ghi chú</label>
+                                    <div class="col-sm-3"><input type="text" name="instructions" class="form-control" style="width: auto;"></div>
+                                    <div class="col-sm-3">
                                         @if (!empty($message_order))
                                             <button class="btn btn-success" disabled type="submit">Đặt cơm</button> 
                                             <span class="alert alert-danger">( {{ $message_order }} )</span>
@@ -63,9 +39,44 @@
                                         @endif
                                     </div>
                                 </div>
-                            </form>
 
+                                <div class="hr-line-dashed"></div>
+                                <br>
+
+                                <ul class="list-group">
+                                    @foreach ($product_rice as $key_product => $product)
+                                    <li class="list-group-item" style="width: 50%;display: inline-block;float: left;">
+                                        <h4>{{ $key_product }}</h4>
+                                        @foreach ($product as $value)
+                                        <div>
+                                            <label class="row" style="display: block;margin-bottom: 10px;display: flex;align-items: center;border: 1px solid #bbb;" for="product_{{ $value->id }}">
+                                                <div class="col-md-1">
+                                                    @if ($product_first->dish_type_name == $value->dish_type_name)
+                                                        <input type="radio" value="{{ $value->id }}" id="product_{{ $value->id }}" name="product_rice">
+                                                    @else
+                                                        <input type="checkbox" name="toppings[]" value="{{ $value->id }}" id="product_{{ $value->id }}">
+                                                    @endif
+                                                </div>
+                                                <div class="col-md-3">
+                                                    <div style="width: 70px;"><img src="{{ $value->dish_photo }}" alt="" style="width: 100%; border-radius: 5px;"></div>
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <h4><strong>{{ $value->name }}</strong></h4>
+                                                    <p>
+                                                        <span style="color: red; font-size: 18px;">
+                                                            ({{ number_format($value->price, 0, ".", ",") . "đ" }})
+                                                        </span>
+                                                    </p>
+                                                </div>
+                                            </label>
+                                        </div>
+                                        @endforeach
+                                    </li>
+                                    @endforeach
+                                </ul>
+                            </form>
                         </div>
+
                     </div>
                 </div>
             </div>
